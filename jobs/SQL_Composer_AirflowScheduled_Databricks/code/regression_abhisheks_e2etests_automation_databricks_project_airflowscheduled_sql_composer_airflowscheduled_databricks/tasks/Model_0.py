24e19d0dd3e7bc65258d25e52728695840560a07
abhisheksr8@gmail.com <abhisheksr8@gmail.com>
2025-05-05 09:36:42 +0000

    Initialize Databricks project with Airflow-scheduled jobs and DBT configurations.

---
+++ jobs/SQL_Composer_AirflowScheduled_Databricks/code/regression_abhisheks_e2etests_automation_databricks_project_airflowscheduled_sql_composer_airflowscheduled_databricks/tasks/Model_0.py
@@ -0,0 +1,40 @@
+from regression_abhisheks_e2etests_automation_databricks_project_airflowscheduled_sql_composer_airflowscheduled_databricks.utils import *
+
+def Model_0():
+    from airflow.operators.python import PythonOperator
+    from datetime import timedelta
+    import os
+    import zipfile
+    import tempfile
+
+    return PythonOperator(
+        task_id = "Model_0",
+        python_callable = invoke_dbt_runner,
+        op_kwargs = {
+          "is_adhoc_run_from_same_project": False,
+          "is_prophecy_managed": False,
+          "run_deps": False,
+          "run_seeds": True,
+          "run_parents": False,
+          "run_children": False,
+          "run_tests": True,
+          "run_mode": "project",
+          "entity_kind": None,
+          "entity_name": None,
+          "project_id": "15772",
+          "git_entity": "branch",
+          "git_entity_value": "main",
+          "git_ssh_url": "https://github.com/abhisheksr8/scheduledJobs_Databricks.git",
+          "git_sub_path": "",
+          "select": "",
+          "threads": "",
+          "exclude": "",
+          "run_props": " --profile run_profile",
+          "envs": {
+            "DBT_DATABRICKS_INVOCATION_ENV": "prophecy", 
+            "DBT_PROFILES_DIR": "/home/airflow/gcs/data", 
+            "DBT_SEND_ANONYMOUS_USAGE_STATS": "false", 
+            "DBT_FULL_REFRESH": "true"
+          }
+        },
+    )
---
+++ models/testmodel/testModelSchdRun_Automation_Databricks_Project_AirflowScheduled.sql
@@ -0,0 +1,11 @@
+WITH testSeedSchdRun_Automation_Databricks_Project_AirflowScheduled AS (
+
+  SELECT * 
+  
+  FROM {{ ref('testSeedSchdRun_Automation_Databricks_Project_AirflowScheduled')}}
+
+)
+
+SELECT *
+
+FROM testSeedSchdRun_Automation_Databricks_Project_AirflowScheduled
